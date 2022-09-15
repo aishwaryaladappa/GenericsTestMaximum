af4fd42ff525b413c90607b379bd213eac6ca4f0
aishwaryaladappa <aishwaryaladapppa971@gmail.com>
2022-09-15 16:40:48 +0530

    added findmaxfloat

---
+++ FindMaxFloatNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsFindMaximum
+{
+    public class FindMaxFloatNumber
+    {
+        public static double MaxFloatNumber(double firstValues, double secondValues, double thirdValues)
+        {
+
+            if (firstValues.CompareTo(secondValues) > 0 && firstValues.CompareTo(thirdValues) > 0)
+            {
+                return firstValues;
+            }
+            if (secondValues.CompareTo(firstValues) > 0 && secondValues.CompareTo(thirdValues) > 0)
+            {
+                return secondValues;
+            }
+            if (thirdValues.CompareTo(firstValues) > 0 && secondValues.CompareTo(firstValues) > 0)
+            {
+                return thirdValues;
+            }
+            throw new Exception("firstnumber,secondnumber and thirdnumber are same");
+        }
+    }
+}
+
